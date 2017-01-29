6d573e8f38d325bbc062033cecd77b99a0094352
dharmic6 <dharmic7@gmail.com>
2017-01-28 19:45:17 -0800

    Missing Number

---
+++ src/com/test/inheritence/MissingNumber.java
@@ -0,0 +1,37 @@
+package com.test.inheritence;
+
+import java.util.Scanner;
+
+public class MissingNumber {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println("Enter the numbers placed");
+		Scanner s = new Scanner(System.in);
+		
+		int count = s.nextInt();
+		
+		
+		int arr[] = new int[count]; // arr[1]
+		
+		System.out.println("Enter the "+(count-1)+" numbers available in the bag pressing the Enter key");
+		
+		
+		for(int i = 0; i < count-1; i++){
+			int val = s.nextInt();
+			arr[val-1] = 1;
+		}
+		
+		
+		for(int i = 0; i < count-1; i++){
+			if(arr[i] == 0){
+				System.out.println("Missing Number : "+(i+1));
+			}
+		}
+		
+		
+		
+	}
+
+}
